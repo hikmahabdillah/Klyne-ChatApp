8bc7785ef8c66dac50204d9a531da3a233ad81bd
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-12-31 09:26:12 +0700

    backend setup

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const login = (req, res) => {
+  res.send("login router");
+}
+export const signup = (req, res) => {
+  res.send("signup router");
+}
+export const logout = (req, res) => {
+  res.send("logout router");
+}

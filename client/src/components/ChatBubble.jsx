ac20834579a6dcec5a84a6f4dfc579c2ea0371d9
hikmahabdillah <hikmahaldrin44@gmail.com>
2025-02-06 18:09:30 +0700

    feat(chat): add automation scrollbottom on chat & add space for between chat for accessibility

--- client/src/components/ChatBubble.jsx
+++ client/src/components/ChatBubble.jsx
@@ -1,23 +1,28 @@
-const ChatBubble = ({ sendTime, chat, typeChat }) => {
+const ChatBubble = ({ sendTime, chat, typeChat, isSameSenderAsPrevious }) => {
+  console.log(isSameSenderAsPrevious);
   if (typeChat === "receive") {
     return (
-      <div className="chat chat-start">
-        <div className="chat-header">
-          <time className="text-xs opacity-70">{sendTime}</time>
-        </div>
-        <div className="chat-bubble font-medium bg-primary text-primary-content">
+      <div
+        className={`chat ${
+          isSameSenderAsPrevious ? "!mt-0 !pt-0" : "!mt-2"
+        } chat-start`}
+      >
+        <div className="chat-bubble font-medium bg-primary text-primary-content flex">
           {chat}
+          <time className="ml-2 self-end text-xs opacity-70">{sendTime}</time>
         </div>
       </div>
     );
   } else if (typeChat === "send") {
     return (
-      <div className="chat chat-end flex flex-col">
-        <div className="chat-header">
-          <time className="text-xs opacity-70">{sendTime}</time>
-        </div>
-        <div className="px-4 py-2 max-w-[90%] w-fit min-w-11 block relative rounded-xl rounded-br-none text-slate-50 bg-gradient-to-r from-[#FF00E5] to-[#794CEB] font-medium">
+      <div
+        className={`chat ${
+          isSameSenderAsPrevious ? "!mt-0 !pt-0" : "!mt-2"
+        } chat-end`}
+      >
+        <div className="px-4 py-2 max-w-[90%] w-fit min-w-11 relative rounded-xl rounded-br-none text-slate-50 bg-gradient-to-r from-[#FF00E5] to-[#794CEB] font-medium flex">
           {chat}
+          <time className="ml-2 self-end text-xs opacity-70">{sendTime}</time>
         </div>
       </div>
     );
--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,9 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useChatStore } from "../store/useChatStore";
 import ChatBubble from "./ChatBubble";
 
 const ChatContainer = () => {
   const { selectedUser, messages, getMessages } = useChatStore();
+  const messagesEndRef = useRef(null);
+
+  useEffect(() => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollTop = messagesEndRef.current.scrollHeight;
+    }
+  }, [messages]);
+
   const formatTime = (isoString) => {
     const date = new Date(isoString);
     return date.toLocaleTimeString("en-US", {
@@ -12,7 +20,6 @@ const ChatContainer = () => {
       hour12: false,
     });
   };
-  console.log(selectedUser);
 
   useEffect(() => {
     if (selectedUser) {
@@ -21,17 +28,26 @@ const ChatContainer = () => {
   }, [selectedUser, getMessages]);
 
   return (
-    <div className="flex-1 overflow-y-auto w-full p-4 space-y-4">
-      {messages.map((msg, index) => (
-        <ChatBubble
-          key={index}
-          typeChat={
-            msg.senderId === selectedUser?.contactRef ? "receive" : "send"
-          }
-          sendTime={formatTime(msg.createdAt)}
-          chat={msg.text}
-        />
-      ))}
+    <div
+      className="flex-1 overflow-y-auto w-full p-4 space-y-4"
+      ref={messagesEndRef}
+    >
+      {messages.map((msg, index) => {
+        const isSameSenderAsPrevious =
+          index > 0 && messages[index - 1]?.senderId === msg.senderId;
+
+        return (
+          <ChatBubble
+            key={index}
+            typeChat={
+              msg.senderId === selectedUser?.contactRef ? "receive" : "send"
+            }
+            sendTime={formatTime(msg.createdAt)}
+            chat={msg.text}
+            isSameSenderAsPrevious={isSameSenderAsPrevious} // True jika pengirimnya sama dengan pesan sebelumnya
+          />
+        );
+      })}
     </div>
   );
 };
